698740c94fa37a9f8c4b16fd8ac11ee92b1a0d40
Trey <treytiller@outlook.com>
2019-08-25 17:57:32 -0400

    backup

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 'use strict'
 
-/*SCRIPT FOR ALL FUNCTIONING BUTTONS*/
+const tdApiKey = '344207-MovieGen-7ZJ76CWA';
+const tasteDive = 'https://tastedive.com/api/similar';
 
-/*HOME SCREEN*/
+/*SCRIPT FOR ALL FUNCTIONING BUTTONS*/
 
 $('.title').on('click', function() {
     //alert('it worked');
@@ -24,6 +25,30 @@ $('.start-over').on('click', function () {
     $('.home-screen').removeClass('hidden');
     $('.movie-title-screen').addClass('hidden');
 });
+
+/*TITLE FETCH REQUEST*/
+
+function watchTitleForm() {
+    $('.user-title-form').submit(event => {
+        event.preventDefault();
+        let title = $('.search-bar').val("");
+        console.log(`Im Working: ${title}`);
+        getMovies(title);
+    });
+}
+
+function getMovies(title){
+    fetch("https://tastedive.com/api/similar" + '?' + `q=${title}` + '&' + `k=344207-MovieGen-7ZJ76CWA`, {mode: 'no-cors'})
+    //.then(response => response.json())
+    .then(responseJson => console.log(responseJson))
+}
+
+
+
+
+
+
+$(watchTitleForm)
 
 
 
